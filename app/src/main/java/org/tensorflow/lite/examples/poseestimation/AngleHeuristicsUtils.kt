1ff05d765f042c999820496923a0ae9e90d854d1
boladss <anton.boladss@gmail.com>
2025-02-10 00:23:36 +0800

    feat: increase degrees of freedom for form checking
    prior values still seem too strict :/

--- app/src/main/java/org/tensorflow/lite/examples/poseestimation/AngleHeuristicsUtils.kt
+++ app/src/main/java/org/tensorflow/lite/examples/poseestimation/AngleHeuristicsUtils.kt
@@ -10,11 +10,11 @@ object AngleHeuristicsUtils {
   private const val STANDARD_UPPER_TORSO_ANGLE = 180;
   private const val STANDARD_UPPER_TORSO_DOF = 10;
   private const val STANDARD_LOWER_TORSO_ANGLE = 180;
-  private const val STANDARD_LOWER_TORSO_DOF = 10;
+  private const val STANDARD_LOWER_TORSO_DOF = 15;
   private const val STANDARD_ELBOW_ANGLE = 180;
-  private const val STANDARD_ELBOW_DOF = 10; // degrees of freedom --- INCREASED TO 10
+  private const val STANDARD_ELBOW_DOF = 20; // degrees of freedom --- INCREASED TO 15
   private const val STANDARD_KNEE_ANGLE = 180;
-  private const val STANDARD_KNEE_DOF = 10;
+  private const val STANDARD_KNEE_DOF = 25;
 
   // ARMS / ELBOWS
   // Checks if within valid range of motion
