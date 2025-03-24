e8b88d529476b0512bd207a97d412426f1ef3ea5
boladss <anton.boladss@gmail.com>
2025-03-24 11:26:08 +0800

    feat: proper deletion of session (delete any corresponding repetitions first)

--- app/src/main/java/org/tensorflow/lite/examples/poseestimation/sessions/DatabaseHandler.kt
+++ app/src/main/java/org/tensorflow/lite/examples/poseestimation/sessions/DatabaseHandler.kt
@@ -118,16 +118,21 @@ class DatabaseHandler(private val context: Context): SQLiteOpenHelper(context, D
 
     fun deleteSessionData(id: Long): Int {
         val db = writableDatabase
-        val deletedId = db.delete(SESSIONS_TABLE_NAME, "$SESSIONS_COL_ID=?", arrayOf(id.toString()))
+
+        // Before deleting a session, must delete all corresponding repetitions with the same sessionId
+        val deletedRepetitions = db.delete(REPS_TABLE_NAME, "$REPS_COL_ID=?", arrayOf(id.toString()))
+
+        // Delete session
+        val deletedSession = db.delete(SESSIONS_TABLE_NAME, "$SESSIONS_COL_ID=?", arrayOf(id.toString()))
 
         // Notify user of updates
-        if (deletedId > 0) {
+        if (deletedSession > 0) {
             Toast.makeText(context, "Session deleted successfully (ID $id)", Toast.LENGTH_SHORT).show()
         } else {
             Toast.makeText(context, "Session failed to delete", Toast.LENGTH_SHORT).show()
         }
 
-        return deletedId
+        return deletedSession
     }
 
     // REPETITION TABLE FUNCTIONS
