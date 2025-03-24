0123d63a3b623ae0b23882147c15d6d3f96eedf8
boladss <anton.boladss@gmail.com>
2025-03-24 11:20:58 +0800

    feat: convert menu into ExpandableListView, now displays reps per session

--- app/src/main/java/org/tensorflow/lite/examples/poseestimation/SessionMenuActivity.kt
+++ app/src/main/java/org/tensorflow/lite/examples/poseestimation/SessionMenuActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.text.InputType
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ExpandableListView
 import android.widget.ListView
 import android.widget.SimpleCursorAdapter
 import androidx.activity.enableEdgeToEdge
@@ -12,9 +13,16 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import org.tensorflow.lite.examples.poseestimation.sessions.DatabaseHandler
 import org.tensorflow.lite.examples.poseestimation.data.ProgressionType
+import org.tensorflow.lite.examples.poseestimation.sessions.RepetitionItem
 import org.tensorflow.lite.examples.poseestimation.sessions.SessionCursorAdapter
+import org.tensorflow.lite.examples.poseestimation.sessions.SessionHeader
 import java.time.Duration
 import java.time.Instant
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
+
+
 
 class SessionMenuActivity : AppCompatActivity() {
 
@@ -49,10 +57,52 @@ class SessionMenuActivity : AppCompatActivity() {
 
     private fun displaySessionData() {
         val cursor = dbHandler.readSessionData()
-        val adapter = SessionCursorAdapter(this, cursor)
-        val sessionListView = findViewById<ListView>(R.id.sessionListView)
-        sessionListView.adapter = adapter
-    }
+//        dbHandler.insertRepetitionData(2, 1, true)
+//        dbHandler.insertRepetitionData(2, 2, true)
+//        dbHandler.insertRepetitionData(2, 3, false)
+//        val adapter = SessionCursorAdapter(this, cursor)
+        val sessionListView = findViewById<ExpandableListView>(R.id.sessionListView)
+//        sessionListView.adapter = adapter
+
+        val header: MutableList<SessionHeader> = ArrayList() // Sessions
+        val childItem: MutableList<MutableList<RepetitionItem>> = ArrayList() // Repetitions
+
+        // Go through each session
+        if (cursor.moveToFirst()) {
+            do {
+                // Get values from entry
+                val sessionId = cursor.getLong(cursor.getColumnIndexOrThrow(DatabaseHandler.SESSIONS_COL_ID))
+                val startTimeISO = cursor.getString(cursor.getColumnIndexOrThrow(DatabaseHandler.SESSIONS_COL_START_TIME))
+                val endTimeISO =  cursor.getString(cursor.getColumnIndexOrThrow(DatabaseHandler.SESSIONS_COL_END_TIME))
+                val progType = cursor.getString(cursor.getColumnIndexOrThrow(DatabaseHandler.SESSIONS_COL_PROG_TYPE))
+
+                val sessionHeader = SessionHeader(sessionId, startTimeISO, endTimeISO, progType)
+                header.add(sessionHeader)
+
+                // Obtain repetitions per session
+                val repetitions: MutableList<RepetitionItem> = ArrayList()
+                val repetitionCursor = dbHandler.readRepetitionData(sessionId)
+
+                if (repetitionCursor.moveToFirst()) {
+                    do {
+                        val repId = repetitionCursor.getLong(repetitionCursor.getColumnIndexOrThrow(DatabaseHandler.REPS_COL_ID))
+                        val repCount = repetitionCursor.getInt(repetitionCursor.getColumnIndexOrThrow(DatabaseHandler.REPS_COL_REP_NUM))
+                        val goodQual = repetitionCursor.getInt(repetitionCursor.getColumnIndexOrThrow(DatabaseHandler.REPS_COL_GOOD_QUAL)) == 1 // Acts as boolean
+
+                        val repetitionItem = RepetitionItem(repId, repCount, goodQual)
+                        repetitions.add(repetitionItem)
+
+                    } while (repetitionCursor.moveToNext())
+                }
+                repetitionCursor.close()
+                childItem.add(repetitions)
+
+            } while (cursor.moveToNext())
+        }
+        cursor.close()
+
+        sessionListView.setAdapter(SessionCursorAdapter(this, header, childItem))
+     }
 
     private fun createNewSession() {
         // Temporary values
--- app/src/main/java/org/tensorflow/lite/examples/poseestimation/sessions/SessionCursorAdapter.kt
+++ app/src/main/java/org/tensorflow/lite/examples/poseestimation/sessions/SessionCursorAdapter.kt
@@ -2,9 +2,11 @@ package org.tensorflow.lite.examples.poseestimation.sessions
 
 import android.content.Context
 import android.database.Cursor
+import android.text.Layout
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.BaseExpandableListAdapter
 import android.widget.TextView
 import androidx.cursoradapter.widget.CursorAdapter
 import org.tensorflow.lite.examples.poseestimation.R
@@ -16,41 +18,161 @@ import java.time.format.DateTimeFormatter
 import java.util.Date
 import java.util.Locale
 
-class SessionCursorAdapter(context: Context, cursor: Cursor?): CursorAdapter(context, cursor, 0) {
+class SessionCursorAdapter(
+    var context: Context,
+    private var header: MutableList<SessionHeader>,
+    private var childItem: MutableList<MutableList<RepetitionItem>>
+): BaseExpandableListAdapter() {
 
     private val inflater: LayoutInflater = LayoutInflater.from(context)
 
-    override fun newView(context: Context, cursor: Cursor, parent: ViewGroup): View {
-        return inflater.inflate(R.layout.session_list_item, parent, false)
+//    override fun newView(context: Context, cursor: Cursor, parent: ViewGroup): View {
+//        return inflater.inflate(R.layout.session_list_item, parent, false)
+//    }
+//
+//    override fun bindView(view: View, context: Context, cursor: Cursor) {
+//        // Views
+//        val textId = view.findViewById<TextView>(R.id.textId)
+//        val textStartTime = view.findViewById<TextView>(R.id.textStartTime)
+//        val textEndTime = view.findViewById<TextView>(R.id.textEndTime)
+//        val textProgressionType = view.findViewById<TextView>(R.id.textProgressionType)
+//
+//        // Get values from entry
+//        val id = cursor.getLong(cursor.getColumnIndexOrThrow(DatabaseHandler.SESSIONS_COL_ID))
+//        val startTimeISO = cursor.getString(cursor.getColumnIndexOrThrow(DatabaseHandler.SESSIONS_COL_START_TIME))
+//        val endTimeISO = cursor.getString(cursor.getColumnIndexOrThrow(DatabaseHandler.SESSIONS_COL_END_TIME))
+//        val progType = cursor.getString(cursor.getColumnIndexOrThrow(DatabaseHandler.SESSIONS_COL_PROG_TYPE))
+//
+//        // Format timestamps
+//        val dateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault())
+//        val startTime = "Started: ${iso8601ToFormat(startTimeISO)}"
+//        val endTime = "Ended: ${iso8601ToFormat(endTimeISO)}"
+//        val idNumber = "ID: $id"
+//
+//        textProgressionType.text = progType
+//        textId.text = idNumber
+//        textStartTime.text = startTime
+//        textEndTime.text = endTime
+//
+//    }
+
+    // Necessary methods for ExpandableAdapter
+    override fun getGroupCount(): Int {
+        return header.size
     }
 
-    override fun bindView(view: View, context: Context, cursor: Cursor) {
-        // Views
-        val textId = view.findViewById<TextView>(R.id.textId)
-        val textStartTime = view.findViewById<TextView>(R.id.textStartTime)
-        val textEndTime = view.findViewById<TextView>(R.id.textEndTime)
-        val textProgressionType = view.findViewById<TextView>(R.id.textProgressionType)
-
-        // Get values from entry
-        val id = cursor.getLong(cursor.getColumnIndexOrThrow(DatabaseHandler.SESSIONS_COL_ID))
-        val startTimeISO = cursor.getString(cursor.getColumnIndexOrThrow(DatabaseHandler.SESSIONS_COL_START_TIME))
-        val endTimeISO = cursor.getString(cursor.getColumnIndexOrThrow(DatabaseHandler.SESSIONS_COL_END_TIME))
-        val progType = cursor.getString(cursor.getColumnIndexOrThrow(DatabaseHandler.SESSIONS_COL_PROG_TYPE))
-
-        // Format timestamps
-        val dateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault())
-        val startTime = "Started: ${iso8601ToFormat(startTimeISO)}"
-        val endTime = "Ended: ${iso8601ToFormat(endTimeISO)}"
-        val idNumber = "ID: $id"
-
-        textProgressionType.text = progType
-        textId.text = idNumber
-        textStartTime.text = startTime
-        textEndTime.text = endTime
-
+    override fun getChildrenCount(p0: Int): Int {
+        return childItem[p0].size
     }
 
-    fun iso8601ToFormat(iso8601String: String, zoneId: ZoneId = ZoneId.systemDefault()): String? {
+    override fun getGroup(p0: Int): Any {
+        return header[p0]
+    }
+
+    override fun getChild(p0: Int, p1: Int): Any {
+        return childItem[p0][p1]
+    }
+
+    override fun getGroupId(p0: Int): Long {
+        return p0.toLong()
+    }
+
+    override fun getChildId(p0: Int, p1: Int): Long {
+        return p1.toLong()
+    }
+
+    override fun hasStableIds(): Boolean {
+        return false
+    }
+
+    override fun getGroupView(p0: Int, p1: Boolean, p2: View?, p3: ViewGroup?): View {
+        var convertView = p2
+
+        if (convertView == null) {
+            val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+            convertView = inflater.inflate(R.layout.session_list_item, null)
+        }
+
+        val sessionData = getGroup(p0) as? SessionHeader
+
+        if (sessionData != null) {
+            // Views
+            val textId = convertView!!.findViewById<TextView>(R.id.textId)
+            val textStartTime = convertView.findViewById<TextView>(R.id.textStartTime)
+            val textEndTime = convertView.findViewById<TextView>(R.id.textEndTime)
+            val textProgressionType = convertView.findViewById<TextView>(R.id.textProgressionType)
+
+            // Get values from entry
+            val sessionId = sessionData.id
+            val startTimeISO = sessionData.startTime
+            val endTimeISO = sessionData.endTime
+            val progType = sessionData.progressionType
+
+            // Format timestamps and text
+//            val dateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault())
+            val startTime = "Started: ${iso8601ToFormat(startTimeISO)}"
+            val endTime = "Ended: ${iso8601ToFormat(endTimeISO)}"
+            val idNumber = "ID: $sessionId"
+
+            textProgressionType.text = progType
+            textId.text = idNumber
+            textStartTime.text = startTime
+            textEndTime.text = endTime
+
+        } else {
+            val textId = convertView!!.findViewById<TextView>(R.id.textId)
+            val errorText = "Error: Session data is null"
+            textId.text = errorText
+        }
+
+        return convertView
+    }
+
+    override fun getChildView(p0: Int, p1: Int, p2: Boolean, p3: View?, p4: ViewGroup?): View {
+        var convertView = p3
+
+        if (convertView == null) {
+            val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+            convertView = inflater.inflate(R.layout.repetition_list_item, null)
+        }
+
+        val repetitionData = getChild(p0, p1) as? RepetitionItem
+
+        if (repetitionData != null) {
+            // Views
+            val textRepId = convertView!!.findViewById<TextView>(R.id.textRepId)
+            val textRepCount = convertView.findViewById<TextView>(R.id.textRepCount)
+            val textRepQuality = convertView.findViewById<TextView>(R.id.textRepQuality)
+
+            // Get values from entry
+            val repId = repetitionData.id
+            val repCount = repetitionData.repCount
+            val repQuality = repetitionData.goodQuality
+
+            // Format text
+            val idNumber = "(ID: $repId)"
+            val repCountText = "Rep #$repCount"
+            val repQualityText = if (repQuality) "Good" else "Bad"
+
+            textRepId.text = idNumber
+            textRepCount.text = repCountText
+            textRepQuality.text = repQualityText
+
+        } else {
+            val textRepId = convertView!!.findViewById<TextView>(R.id.textRepId)
+            val errorText = "Error: Repetition data is null"
+            textRepId.text = errorText
+        }
+
+        return convertView
+    }
+
+    override fun isChildSelectable(p0: Int, p1: Int): Boolean {
+        return true
+    }
+
+    // Helper function to translate timestamps
+    private fun iso8601ToFormat(iso8601String: String, zoneId: ZoneId = ZoneId.systemDefault()): String? {
         return try {
             val instant = Instant.parse(iso8601String)
             val localDateTime = LocalDateTime.ofInstant(instant, zoneId)
