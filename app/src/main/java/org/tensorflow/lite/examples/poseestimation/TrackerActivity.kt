fac8b2e2c2b9dd1a57625ab5fd6bd6274890ac8a
boladss <anton.boladss@gmail.com>
2025-05-21 00:58:18 +0800

    feat: add toggle for beeping, remove pose classifier

--- app/src/main/java/org/tensorflow/lite/examples/poseestimation/TrackerActivity.kt
+++ app/src/main/java/org/tensorflow/lite/examples/poseestimation/TrackerActivity.kt
@@ -54,6 +54,9 @@ class TrackerActivity : AppCompatActivity() {
     /** Default device is GPU */
     private var device = Device.GPU
 
+    /** Beeps disabled by default */
+    private var repetitionAudio = false
+
     var persons = listOf<Person>()
     private lateinit var dbHandler: DatabaseHandler
     private lateinit var tvScore: TextView
@@ -63,7 +66,9 @@ class TrackerActivity : AppCompatActivity() {
     private lateinit var spnTracker: Spinner
     private lateinit var vTrackerOption: View
     private lateinit var swSkeleton: SwitchCompat
-    private lateinit var vSkeletonOption: View // RelativeLayout
+    private lateinit var vSkeletonOption: View
+    private lateinit var swAudio: SwitchCompat
+    private lateinit var vAudioOption: View
     private lateinit var repFeedback: TextView
     private lateinit var repCounter: TextView
     private lateinit var displayProgressionType: TextView
@@ -127,6 +132,11 @@ class TrackerActivity : AppCompatActivity() {
             showSkeleton(isChecked)
         }
 
+    private var setAudioListener =
+        CompoundButton.OnCheckedChangeListener { _, isChecked ->
+            toggleAudio(isChecked)
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tracker)
@@ -148,13 +158,23 @@ class TrackerActivity : AppCompatActivity() {
         spnTracker = findViewById(R.id.spnTracker)
         vTrackerOption = findViewById(R.id.vTrackerOption)
         surfaceView = findViewById(R.id.surfaceView)
-        swSkeleton = findViewById(R.id.swSkeleton)
-        vSkeletonOption = findViewById(R.id.vSkeletonOption) // RelativeLayout
+
+        // Switch for skeleton overlay
+        swSkeleton = findViewById<SwitchCompat>(R.id.swSkeleton)
+        vSkeletonOption = findViewById<View>(R.id.vSkeletonOption) // RelativeLayout
+
+        // Switch for beeping
+        swAudio = findViewById<SwitchCompat>(R.id.swAudio)
+        vAudioOption = findViewById<View>(R.id.vAudioOption) // RelativeLayout
+
         repFeedback = findViewById(R.id.tvRepFeedback)
         repCounter = findViewById(R.id.tvRepCounter)
         initSpinner()
         spnModel.setSelection(modelPos)
+
         swSkeleton.setOnCheckedChangeListener(setSkeletonListener)
+        swAudio.setOnCheckedChangeListener(setAudioListener)
+
         if (!isCameraPermissionGranted()) {
             requestPermission()
         }
@@ -242,8 +262,9 @@ class TrackerActivity : AppCompatActivity() {
 
         }
         currentState = nextState
-        if (currentState.state == ProgressionStates.GOINGUP &&
-            !mediaPlayer.isPlaying) {
+
+        // Beeping per repetition denoting good or poor performance
+        if (repetitionAudio && currentState.state == ProgressionStates.GOINGUP && !mediaPlayer.isPlaying) {
             mediaPlayer.reset()
             if (currentState.goodForm)
                 mediaPlayer.setDataSource(resources.openRawResourceFd(R.raw.goodform))
@@ -372,21 +393,18 @@ class TrackerActivity : AppCompatActivity() {
         val poseDetector = when (modelPos) {
             0 -> {
                 // MoveNet Lightning (SinglePose)
-                showPoseClassifier(true)
                 showDetectionScore(true)
                 showTracker(false)
                 MoveNet.create(this, device, ModelType.Lightning)
             }
             1 -> {
                 // MoveNet Thunder (SinglePose)
-                showPoseClassifier(true)
                 showDetectionScore(true)
                 showTracker(false)
                 MoveNet.create(this, device, ModelType.Thunder)
             }
             2 -> {
                 // MoveNet (Lightning) MultiPose
-                showPoseClassifier(false)
                 showDetectionScore(false)
                 // Movenet MultiPose Dynamic does not support GPUDelegate
                 if (device == Device.GPU) {
@@ -401,7 +419,6 @@ class TrackerActivity : AppCompatActivity() {
             }
             3 -> {
                 // PoseNet (SinglePose)
-                showPoseClassifier(true)
                 showDetectionScore(true)
                 showTracker(false)
                 PoseNet.create(this, device)
@@ -415,23 +432,19 @@ class TrackerActivity : AppCompatActivity() {
         }
     }
 
-    // Show/hide the pose classification option. -> RelativeLayout
-    private fun showPoseClassifier(isVisible: Boolean) {
-        vSkeletonOption.visibility = if (isVisible) View.VISIBLE else View.GONE
-        if (!isVisible) {
-            swSkeleton.isChecked = false
-        }
-    }
-
     // Show/hide the detection score.
     private fun showDetectionScore(isVisible: Boolean) {
         tvScore.visibility = if (isVisible) View.VISIBLE else View.GONE
     }
 
-    // Show/hide classification result.
+    // Show/hide skeleton overlay
     private fun showSkeleton(isVisible: Boolean) {
-        val visibility = if (isVisible) View.VISIBLE else View.GONE
+//        val visibility = if (isVisible) View.VISIBLE else View.GONE
         VisualizationUtils.skeletonOverlay = isVisible
+    }
+
+    private fun toggleAudio(isEnabled: Boolean) {
+        repetitionAudio = isEnabled
     }
 
     // Show/hide the tracking options.
